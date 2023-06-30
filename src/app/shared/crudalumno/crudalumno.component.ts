9f4d89ce96b57929192eadf21887e1942dda5fb5
JennyFer1810 <110933933+JennyFer1810@users.noreply.github.com>
2023-06-29 22:00:02 -0500

    CrudAlumno-CrudDocentes

--- src/app/shared/crudalumno/crudalumno.component.ts
+++ src/app/shared/crudalumno/crudalumno.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable, map, startWith } from 'rxjs';
+import { Student } from 'src/app/models/studentmodel';
 
 @Component({
   selector: 'app-crudalumno',
@@ -34,3 +35,39 @@ export class CrudalumnoComponent {
       option.toLowerCase().indexOf(val.toLowerCase()) === 0);
   }
 }
+/*
+function addStudent(): void{
+  let currentStudent:Student = {
+    id: readFromHtml("cedula_alumno"),
+    nombre: readFromHtml("nombre_alumno"),
+    apellido: readFromHtml("apellido_alumno"),
+    fechanacimiento: undefined,
+    contacto: readFromHtml("telefono_alumno"),
+    direccion: readFromHtml("direccion_alumno"),
+    email: readFromHtml("email_alumno"),
+    contraseña: readFromHtml("contraseña_alumno"),
+    idjornada: parseInt (readFromHtml("telefono_alumno")),
+    idnivel: parseInt (readFromHtml("telefono_alumno")),
+    idparalelo: parseInt (readFromHtml("telefono_alumno")),
+    idcarrera: parseInt (readFromHtml("telefono_alumno")),
+  } 
+}
+
+
+id: string;
+    nombre: string;
+    apellido: string;
+    fechanacimiento: Date;
+    contacto: string;
+    direccion: string;
+    email: string;
+    contraseña: string;
+    idjornada: number;
+    idnivel: number;
+    idparalelo: number;
+    idcarrera: number;
+     
+
+    function readFromHtml(id: string):string {
+      return (<HTMLInputElement> document.getElementById(id)).value;
+  }*/
