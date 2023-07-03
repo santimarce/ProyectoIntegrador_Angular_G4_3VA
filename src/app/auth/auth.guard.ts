90e67694968554f4893c5900b11dbf835b496edd
butterspc <rfp.pineda@yavirac.edu.ec>
2023-07-02 19:20:53 -0500

    change guard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  
+}
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -4,13 +4,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { TeachersComponent } from './teachers/teachers.component';
 import { StudentsComponent } from './students/students.component';
 import { AdminComponent } from './admin/admin.component';
-import { authGuard } from '../auth/auth.guard';
+import { AuthGuard } from '../auth/auth.guard';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent, pathMatch: 'full' },
-  { path:'students', component: StudentsComponent, canActivate: [authGuard]},
-  { path: 'teachers', component: TeachersComponent, canActivate: [authGuard] },
-  { path:'admin', component: AdminComponent, canActivate: [authGuard]}
+  { path:'students', component: StudentsComponent, canActivate: [AuthGuard]},
+  { path: 'teachers', component: TeachersComponent, canActivate: [AuthGuard] },
+  { path:'admin', component: AdminComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
